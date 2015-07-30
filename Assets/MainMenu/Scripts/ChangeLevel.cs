59783b957b3c1466636f4b512364edb52bcafad6
Javier Bullrich <javierbullrich@gmail.com>
2015-07-30 14:21:07 -0300

    finish30

--- Assets/MainMenu/Scripts/ChangeLevel.cs
+++ Assets/MainMenu/Scripts/ChangeLevel.cs
@@ -6,4 +6,7 @@ public class ChangeLevel : MonoBehaviour {
 	public void ChangeToScene(string SceneToChangeTo){
 		Application.LoadLevel (SceneToChangeTo);
 	}
+	public void OpenURL() {
+		Application.OpenURL ("https://github.com/Bullrich/ClassicGamesDemo");
+	}
 }
